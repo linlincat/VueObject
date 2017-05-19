933d3b4b0ca3e0ae0e36b58771184ae7480c4107
M <283125476QQ.com>
2017-05-19 10:20:17 +0800

    添加新分支testing，修改Index.vue

--- src/view/Index.vue
+++ src/view/Index.vue
@@ -8,17 +8,33 @@
 
 <script>
 export default {
-  name: 'app'
+  name: 'index',
+  data () {
+    return {
+      msg: 'this is msg'
+    }
+  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
 }
 </style>
+
