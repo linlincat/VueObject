b733d52477ef323fc9c7599dc217f0cc2692c5c0
M <283125476QQ.com>
2017-06-02 08:42:28 +0800

    添加Login模块，晚上框架

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="Login">
+    {{ msg }}
+    {{ count }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+      msg: 'login'
+    }
+  },
+  computed: {
+    count () {
+	    alert(123)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
